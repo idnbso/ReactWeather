403ec84b30d547bbeb0045740cbbf445e178651b
Idan <idanbusso@gmail.com>
2016-12-25 18:36:54 +0200

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,16 +1,11 @@
 let React = require('react');
 
-// let About = React.createClass({
-//     render: () => {
-//         return (
-//             <h3>About Component</h3>
-//         );
-//     }
-// });
-
 let About = props => {
     return (
-        <h3>About Component</h3>
+        <div>
+            <h3>About</h3>
+            <p>Welcome to about page!</p>
+        </div>
     );
 };
 
